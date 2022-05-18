dc73eba04dcf935a4b12043468ac91e02a78e905
benzotrav <93350704+benzotrav@users.noreply.github.com>
2022-05-19 00:42:48 +0300

    task 3 done

---
+++ src/js/03-feedback.js
@@ -0,0 +1,38 @@
+import throttle from "lodash.throttle";
+
+const feedbackForm = document.querySelector('.feedback-form');
+
+const key = "feedback-form-state";
+let formData = {}
+ 
+feedbackForm.addEventListener('submit', onFormSubmit);
+feedbackForm.addEventListener('input', throttle(onInputForm, 500));
+
+onSaveInfoForm()
+
+function onSaveInfoForm() {
+    const savedData = localStorage.getItem(key);
+    if (savedData) {
+        const {email, message} = JSON.parse(savedData);
+        feedbackForm.email.value = formData.email;
+        feedbackForm.message.value = formData.message;
+        formData.email = email;
+        formData.message = message;
+    }
+}
+
+
+function onInputForm(evt) {
+    formData.email = feedbackForm.elements.email.value;
+    formData.message = feedbackForm.elements.message.value;
+    localStorage.setItem(key,JSON.stringify(formData));
+}
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+
+    evt.currentTarget.reset();
+    localStorage.removeItem(key);
+
+    console.log(formData)
+}
